3d49ca8adedfbcffe5fad4d8b98762e695be02bf
Jacson Medeiros <jacsonmedeiros@gmail.com>
2022-05-17 01:34:54 -0300

    Inclusao de novos exercicios

---
+++ javascript/Exercicio_Modulos/assets/js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Jacson", 37));
+console.log(mostraCidade("Jacson", "Teixeira de Freitas"));
+console.log(mostraHobby("Jacson", "Ciclismo"));
